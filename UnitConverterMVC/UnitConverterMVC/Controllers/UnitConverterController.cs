5c0c681caae685c087951b9c3b6b138527d69b4a
Unknown <zachkbc@gmail.com>
2018-06-21 14:57:56 -0500

    lots of init dont pull yet

---
+++ UnitConverterMVC/UnitConverterMVC/Controllers/UnitConverterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UnitConverterMVC.Controllers
+{
+    public class UnitConverterController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(viewName: "Index", model: 0);
+        }
+        
+    }
+}
